8610358a19118c2faf8647b78c9059a214951f18
Nevin <qq2201591870@163.com>
2024-08-09 01:23:50 +0800

    remove unused

--- asphyxia/asphyxia/asphyxia/SocketExtensions.cs
+++ asphyxia/asphyxia/asphyxia/SocketExtensions.cs
@@ -25,16 +25,6 @@ namespace asphyxia
         /// <param name="socketAddress">SocketAddress</param>
         /// <returns>Sent bytes</returns>
         public static int SendTo(this Socket socket, byte[] buffer, int offset, int size, SocketFlags socketFlags, SocketAddress socketAddress) => socket.SendTo(new ReadOnlySpan<byte>(buffer, offset, size), socketFlags, socketAddress);
-
-        /// <summary>
-        ///     Receive from
-        /// </summary>
-        /// <param name="socket">Socket</param>
-        /// <param name="buffer">Buffer</param>
-        /// <param name="socketFlags">SocketFlags</param>
-        /// <param name="socketAddress">SocketAddress</param>
-        /// <returns>Received bytes</returns>
-        public static int ReceiveFrom(this Socket socket, Span<byte> buffer, SocketFlags socketFlags, ref SocketAddress socketAddress) => socket.ReceiveFrom(buffer, socketFlags, socketAddress);
     }
 }
 #endif
